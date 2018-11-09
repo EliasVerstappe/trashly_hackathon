3865bb409502183a6a52e6f813ffdf98e7efce62
Michiel Meyfroodt <michiel.pitou@gmail.com>
2018-11-09 11:47:24 +0100

    Added working app to start from

---
+++ app/shared/sidedrawer.js
@@ -0,0 +1,14 @@
+const frameModule = require("ui/frame");
+
+function traverse(page) {
+    console.log("Traversing to " + page);
+    frameModule.topmost().navigate({
+        moduleName: "pages/" + page,
+        transition: "slideBottom"
+    });
+}
+
+exports.onAbout = function() { traverse('about/about'); }
+exports.onContact = function() { traverse('contactus/contactus'); }
+exports.onProjects = function() { traverse('projects/overview/overview'); }
+exports.onHome = function() { traverse('home/home'); }
