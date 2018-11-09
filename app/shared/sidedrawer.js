c344a77935cfadf1a0bb763bfce33850bf7eaa5a
Michiel Meyfroodt <michiel.pitou@gmail.com>
2018-11-09 16:40:58 +0100

    Working Scanner for Trash

--- app/shared/sidedrawer.js
+++ app/shared/sidedrawer.js
@@ -13,3 +13,4 @@ exports.onContact = function() { traverse('contactus/contactus'); }
 exports.onProjects = function() { traverse('projects/overview/overview'); }
 exports.onHome = function() { traverse('home/home'); }
 exports.onReward = function() { traverse('rewards/rewards'); }
+exports.onScanTrash = function() { traverse('scan_trash/scan_trash'); }
